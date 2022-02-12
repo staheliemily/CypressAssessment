6d08b42a905a8578065fe78b1047ba401ac9a794
Emily Staheli <staheliwebdesign@gmail.com>
2022-02-12 00:33:40 -0700

    validateEmail

--- cypress/integration/get-a-demo.e2e-spec.js
+++ cypress/integration/get-a-demo.e2e-spec.js
@@ -5,7 +5,8 @@
  import { GetADemoPage } from '../pages/get-a-demo.po'
 
  let demoPage = new GetADemoPage()
- let email = 'test@test.com'
+ let gmailEmail = 'helpdesk@gmail.com'
+ let awardcoEmail = 'test@awardco'
  
  describe('Get A Demo Page',()=>{
      beforeEach(()=>{
@@ -21,8 +22,6 @@
         demoPage.inputField("Work Email",'test').should('have.value', 'test');
         demoPage.inputField("Phone Number",'test').should('have.value','test');
         demoPage.inputField("Company Name",'test').should('have.value','test');
-         
-
         //Tests that Calendar can be updated and dropdown is updated correctly
         cy.get('.hs-dateinput').click().get('[data-pika-day="1"]').click();
         cy.get('[name="demo_time"]').select('09:00 AM (EST)').should('contain','09:00 AM (EST)')
@@ -30,6 +29,18 @@
         //TO DO
         //Future Improvement - When putting in an awardco email verify popup, Verify email and phone number tests. 
         // test to submit and verify that API received the call
+     })
+     it('Verify incorrect email entered',()=>{
+         //Verifies that gmail is not entered
+         demoPage.inputField("Work Email",gmailEmail).should('have.value', gmailEmail);
+         demoPage.incorrectEmail();
+
+        //Verifies award.co email entered
+        demoPage.inputField("Work Email",awardcoEmail).should('have.value', awardcoEmail);
+        cy.get('.modal-window').should('have.length', 1);
+        cy.get('.modal-window').should('contain',"It looks like you're already a customer");
+        cy.get('.modal-window > .button').should('have.attr','href','https://awardco.zendesk.com')
+        
      })
      
  })
